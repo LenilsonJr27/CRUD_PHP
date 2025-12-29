779334ea3e5ff7d4ac94612a492d1c1bb64ea139
Lenilson Junior <99227401+LenilsonJr27@users.noreply.github.com>
2025-12-29 17:19:54 -0300

    Tratamento de erros ao cadastrar usuário

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require 'conexao.php';
 ?>
 
@@ -15,6 +16,7 @@ require 'conexao.php';
 <body>
     <?php include('navbar.php'); ?>
     <div class="mt-4">
+        <?php include('mensagem.php'); ?>
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
