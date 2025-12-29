1972fb4801acff334e75ae32dbe52a620f395774
Lenilson Junior <99227401+LenilsonJr27@users.noreply.github.com>
2025-12-29 15:35:47 -0300

    Embrião do projeto

---
+++ CRUD/index.php
@@ -0,0 +1,61 @@
+<?php
+require 'conexao.php';
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Usuários</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+    <div class="mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Lista de usuários
+                            <a href="usuario-create.php" class="btn btn-primary float-end">Adicionar usuário</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <table class="table teble-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th>Data nascimento</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>junior</td>
+                                    <td>junior@gmail.com</td>
+                                    <td>27/11/2003</td>
+                                    <td>
+                                        <a href="" class="btn btn-secondary btn-sm">Visualizar</a>
+                                        <a href="" class="btn btn-success btn-sm">Editar</a>
+                                        <form action="" method="POST" class="d-inline">
+                                            <button type="submit" name="delete" value="1" class="btn btn-danger btn-sm">Excluir</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
